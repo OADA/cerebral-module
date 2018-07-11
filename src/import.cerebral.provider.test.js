4e9e58d08c8e18c7fef3ed158db92c5129000b9c
Servio Palacios <servio@palacios.com>
2018-07-10 22:45:06 -0400

    array-ified - draft 2

--- src/import.cerebral.provider.test.js
+++ src/import.cerebral.provider.test.js
@@ -1,5 +1,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
 import { Provider } from 'cerebral';
+import { props, state } from 'cerebral/tags';
 import oadaModule from '../build/index';
 import * as oada from '../build/sequences';
 
@@ -12,6 +13,10 @@ let contentType = 'application/vnd.oada.yield.1+json';
 
 console.log('oada', oada);
 console.log('oada module', oadaModule);
+
+state.set('oada.domain', 'https://vip3.ecn.purdue.edu');
+state.set('oada.hostname', 'vip3.ecn.purdue.edu');
+//state.set('oada.options');
 
 describe('#connection()', function() {
   it('should connect when a token is provided', () => {
