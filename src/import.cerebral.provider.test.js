72b541c32f78687d8175f1c7f0bbaa5e41caf444
Servio Palacios <servio@palacios.com>
2018-07-11 00:41:12 -0400

    array-ified - test case

--- src/import.cerebral.provider.test.js
+++ src/import.cerebral.provider.test.js
@@ -1,8 +1,10 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
 import { Provider } from 'cerebral';
 import { props, state } from 'cerebral/tags';
+import { CerebralTest } from 'cerebral/test';
 import oadaModule from '../build/index';
 import * as oada from '../build/sequences';
+import assert from 'assert';
 
 import chai from 'chai';
 let expect = chai.expect;
@@ -14,15 +16,30 @@ let contentType = 'application/vnd.oada.yield.1+json';
 console.log('oada', oada);
 console.log('oada module', oadaModule);
 
-state.set('oada.domain', 'https://vip3.ecn.purdue.edu');
-state.set('oada.hostname', 'vip3.ecn.purdue.edu');
+// state.set('oada.domain', 'https://vip3.ecn.purdue.edu');
+// state.set('oada.hostname', 'vip3.ecn.purdue.edu');
 //state.set('oada.options');
-
 describe('#connection()', function() {
-  it('should connect when a token is provided', () => {
-    console.log('');
-    return oada.connect({token, domain: 'https://vip3.ecn.purdue.edu'}).then((result) => {
-      expect(result).to.have.key('token')
+  it('should connect', () => {
+    const cerebral = CerebralTest(oadaModule); // Expects a Module
+
+    cerebral.setState('oada.domain', 'https://vip3.ecn.purdue.edu');
+    cerebral.setState('oada.hostname', 'vip3.ecn.purdue.edu');
+
+    return cerebral.runSignal('connect',
+                            {token, domain: 'https://vip3.ecn.purdue.edu'})
+                            .then(({ state }) => {
+      //assert.(state.token, 1)
+      console.log('state', state);
     })
   })
 });
+
+// describe('#connection2()', function() {
+//   it('should connect when a token is provided', () => {
+//     console.log('');
+//     return oada.connect({token, domain: 'https://vip3.ecn.purdue.edu'}).then((result) => {
+//       expect(result).to.have.key('token')
+//     })
+//   })
+// });
--- test/import.cerebral.provider.test.js
+++ test/import.cerebral.provider.test.js
@@ -2,6 +2,10 @@
 
 var _cerebral = require('cerebral');
 
+var _tags = require('cerebral/tags');
+
+var _test = require('cerebral/test');
+
 var _index = require('../build/index');
 
 var _index2 = _interopRequireDefault(_index);
@@ -9,6 +13,10 @@ var _index2 = _interopRequireDefault(_index);
 var _sequences = require('../build/sequences');
 
 var oada = _interopRequireWildcard(_sequences);
+
+var _assert = require('assert');
+
+var _assert2 = _interopRequireDefault(_assert);
 
 var _chai = require('chai');
 
@@ -29,17 +37,30 @@ var contentType = 'application/vnd.oada.yield.1+json';
 console.log('oada', oada);
 console.log('oada module', _index2.default);
 
-var connection = oada.connect({ token: token, domain: 'https://vip3.ecn.purdue.edu' }).then(function (result) {
-  expect(result).to.have.key('token');
-});
-
-console.log('connection ', connection);
-
+// state.set('oada.domain', 'https://vip3.ecn.purdue.edu');
+// state.set('oada.hostname', 'vip3.ecn.purdue.edu');
+//state.set('oada.options');
 describe('#connection()', function () {
-  it('should connect when a token is provided', function () {
-    console.log('');
-    return oada.connect({ token: token, domain: 'https://vip3.ecn.purdue.edu' }).then(function (result) {
-      expect(result).to.have.key('token');
+  it('should connect', function () {
+    var cerebral = (0, _test.CerebralTest)(_index2.default); // Expects a Module
+
+    cerebral.setState('oada.domain', 'https://vip3.ecn.purdue.edu');
+    cerebral.setState('oada.hostname', 'vip3.ecn.purdue.edu');
+
+    return cerebral.runSignal('connect', { token: token, domain: 'https://vip3.ecn.purdue.edu' }).then(function (_ref) {
+      var state = _ref.state;
+
+      //assert.(state.token, 1)
+      console.log('state', state);
     });
   });
 });
+
+// describe('#connection2()', function() {
+//   it('should connect when a token is provided', () => {
+//     console.log('');
+//     return oada.connect({token, domain: 'https://vip3.ecn.purdue.edu'}).then((result) => {
+//       expect(result).to.have.key('token')
+//     })
+//   })
+// });
