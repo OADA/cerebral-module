277ef7617f33d70850967b4843dbb65b9d23b2a8
Cyrus Bowman <cyrusbow@gmail.com>
2019-06-13 18:49:24 -0400

    Removed module syntax

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
-import { Module } 	from 'cerebral';
 import signals from './sequences';
 import oada	from '@oada/cerebral-provider';
-/*import StorageModule from '@cerebral/storage'
-const storage = StorageModule({
 
-  target: localStorage,
-  json: true,
-  sync: {
-    'connections': 'connections'
-  }
-
-})*/
-
-export default Module ({
+export default {
 
   state: { connections: {} },
 
 	providers: { oada },
 
-  signals,
+  signals
 
-  //  modules: {storage}
-
-})
+}
